81348a15fdd3d38a1ab8e9f82c659b664f7668aa
janismac <janismac>
2015-08-22 14:22:32 +0200

    some comments/documentation

--- js/levels/StabilizeDoublePendulum.js
+++ js/levels/StabilizeDoublePendulum.js
@@ -5,10 +5,10 @@ Levels.StabilizeDoublePendulum = function()
 {
 	this.name = "StabilizeDoublePendulum";
 	this.title = "Inverted Double Pendulum: Stabilize";
-	this.sampleSolution = "function controlFunction(p)\n{\n  var c =[-0.36447, -3.12476, 290.6325, 114.9936, -280.555, -155.5489];\n  return c[0]*p.x + c[1]*p.dx + c[2]*p.theta1 + c[3]*p.dtheta1 + c[4]*p.theta2 + c[5]*p.dtheta2;\n}";
+	this.sampleSolution = "function controlFunction(p)\n{\n  // Controller coefficients optimized using this sage math script:\n  // https://github.com/janismac/ControlChallenges/blob/master/misc/doublependulum.sage\n  \n  var c =[-0.36447, -3.12476, 290.6325, 114.9936, -280.555, -155.5489];\n  return c[0]*p.x + c[1]*p.dx + c[2]*p.theta1 + c[3]*p.dtheta1 + c[4]*p.theta2 + c[5]*p.dtheta2;\n}";
 	this.boilerPlateCode = "function controlFunction(pendulum)\n{\n  return 10*Math.sin(8*pendulum.T);\n}";
 	this.description = "Stabilize the double pendulum so that it stays upright, moves to the center (x=0) and stays there in perfect balance. Calculate the horizontal force on the cart necessary to achieve this.";
-	this.model = new Models.DoublePendulum({m0: 10,m1: 2,m2: 4,L1: 0.618,L2: 1,g: 2,theta1: 0.1,dtheta1: 0.1,theta2: 0.1,dtheta2: 0.1,x: -1,dx: 0,F: 0,T: 0});
+	this.model = new Models.DoublePendulum({m0: 10,m1: 2,m2: 4,L1: 0.618,L2: 1,g: 2,theta1: -0.05,dtheta1: 0,theta2: 0,dtheta2: 0.05,x: -1,dx: 0,F: 0,T: 0});
 }
 
 
