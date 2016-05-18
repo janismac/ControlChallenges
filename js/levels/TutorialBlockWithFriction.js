b762b99fff6a6846d65ae97a4f910d2ade28c136
janismac <janismac@gmx.de>
2016-05-18 16:57:20 +0200

    javascript note

--- js/levels/TutorialBlockWithFriction.js
+++ js/levels/TutorialBlockWithFriction.js
@@ -8,7 +8,7 @@ Levels.TutorialBlockWithFriction = function()
 
 	this.sampleSolution = "function controlFunction(block)\n{\n  // Idea: If the block is on the left (negative x)\n  // it should be pushed to the right (positive force),\n  // and vice versa. The farther away the block is from\n  // its target, the harder it should be pushed.\n  // This is called a 'proportional controller'.\n  \n  return -3*block.x;\n}";
 	this.boilerPlateCode = "function controlFunction(block)\n{\n  return 5*Math.sin(10*block.T);\n}";
-	this.description = "Push the block under the arrow (x=0) and make it stop there. Calculate the horizontal force on the block necessary to achieve this.";
+	this.description = "Push the block under the arrow (x=0) and make it stop there. Write a <u>JavaScript</u> function that calculates the horizontal force on the block necessary to achieve this.";
 	this.model = new Models.BlockOnSlope({g: 0,x: -2,dx: 0,slope: 0,friction: 1});
 }
 
